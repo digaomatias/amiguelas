65f867edee734825960092515538a7ab490946d4
Rodrigo Leote <rodrigomleote@gmail.com>
2017-01-14 18:00:54 -0200

    added a few jasmine tests for the friends-draft.

---
+++ server/tests/business_logic/friends-draft-runner.spec.js
@@ -0,0 +1,27 @@
+
+describe('friends draft runner', () => {
+    //var friendsDraft = new FriendsDraftRunner();
+    
+    it('should not accept duplicate ids', () => {
+        expect(true).toBe(false);
+            
+    });
+
+    it('should not accept less than three items when run', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should return bi-dimensional array of length n for n parameter items, ', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should return bi-dimensional array with all friends appearing only once in position 0 and 1', () => {
+        expect(true).toBe(false);
+    });
+
+    it('should return bi-dimensional array with all array positions fullfiled by two friends (friend 1 got friend 2)', () => {
+        expect(true).toBe(false);
+    });
+        
+});
+    
