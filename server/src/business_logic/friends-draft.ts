0aded072027b3d5853a7a31012f2302c80274de6
Rodrigo Leote <rodrigomleote@gmail.com>
2017-01-17 01:18:05 -0200

    Implemented friends-draft and friends-draft tests. It's still not working thou

--- server/src/business_logic/friends-draft.ts
+++ server/src/business_logic/friends-draft.ts
@@ -1,6 +1,42 @@
+require('../common/tsarray');
+
 export default class FriendsDraft {
     public runDraft(friendsId: number[]): number[][] {
-        return null
+        this.guard(friendsId);
+
+        let draftedFriends: number[][];
+
+        //Secrets gets friends (position 0 and 1 respectively)
+        let secrets: number[] = friendsId.map(v => v);
+        let friends: number[] = friendsId.map(v => v);
+        
+        for(let i = 0; i < friendsId.length; i++) {
+            let secret = this.random(secrets.length);
+            secrets.splice(secrets.indexOf(secret), 1);
+
+            draftedFriends[i][0] = secret;
+
+            let friend = this.random(friends.length);
+            friends.splice(friends.indexOf(friend), 1);
+
+            draftedFriends[i][1] = friend;
+        }
+
+        return draftedFriends;
     }    
+
+    private guard(friendsId: number[]) {
+        if(friendsId.hasDuplicates()) {
+            throw "Duplicate items was provided";
+        }
+
+        if(friendsId.length < 3) {
+            throw "At least 3 friends are needed for draft";
+        }
+    }
+
+    private random(max: number): number {
+        return Math.floor((Math.random() * max));
+    }
 }
 
--- server/tests/business_logic/friends-draft.spec.ts
+++ server/tests/business_logic/friends-draft.spec.ts
@@ -6,23 +6,59 @@ describe('friends draft', () => {
     it('should not accept duplicate ids', () => {
         let ids: number[] = [1, 1, 2, 3];
 
-        expect(function () { friendsDraft.runDraft(ids); }).toThrowError('Duplicate items was provided');
+        expect(function () { friendsDraft.runDraft(ids); }).toThrow('Duplicate items was provided');
     });
 
     it('should not accept less than three items when run', () => {
-        expect(true).toBe(false);
+        let ids: number[] = [1, 1];
+        
+        expect(function () { friendsDraft.runDraft(ids); }).toThrow('At least 3 friends are needed for draft');
     });
 
-    it('should return bi-dimensional array of length n for n parameter items, ', () => {
-        expect(true).toBe(false);
+    it('should return bi-dimensional [n][2] array for an array of length n, ', () => {
+        let ids: number[] = [1, 2, 3, 4];
+        let result: number[][] = friendsDraft.runDraft(ids);        
+        expect(result.length).toBe(4);
+
+        for(let i=0; i < result.length; i++) {
+            expect(result[i].length).toBe(2);
+        }
     });
 
     it('should return bi-dimensional array with all friends appearing only once in position 0 and 1', () => {
-        expect(true).toBe(false);
+        let ids: number[] = [1, 2, 3, 4];
+        let result: number[][] = friendsDraft.runDraft(ids);        
+        
+        let pos1: number = result[0][0];
+        let pos2: number = result[0][1];
+        for(let i=1; i < result.length; i++) {
+            expect(result[i][0]).not.toBe(pos1);
+            expect(result[i][1]).not.toBe(pos2);
+        }
     });
 
     it('should return bi-dimensional array with all array positions fullfiled by two friends (friend 1 got friend 2)', () => {
-        expect(true).toBe(false);
+        let ids: number[] = [1, 2, 3, 4];
+        let result: number[][] = friendsDraft.runDraft(ids);        
+        
+        for(let i=1; i < result.length; i++) {
+            expect(ids.indexOf(result[i][0])).toBeGreaterThanOrEqual(0);
+            expect(ids.indexOf(result[i][1])).toBeGreaterThanOrEqual(0);
+        }
     });
 
+    it('should not repeat results all the time', () => {
+        //Chances of repeating are very very low...
+        let ids: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+        
+        let result1 = friendsDraft.runDraft(ids);
+        let result2 = friendsDraft.runDraft(ids);
+
+        let isRepeating: boolean = true;
+        for(let i = 0; i < result1.length; i++) {
+            isRepeating = isRepeating && result1[i][0] === result2[i][0] && result1[i][1] === result2[i][1]
+        }
+
+        expect(isRepeating).toBeFalsy();
+    })
 });
